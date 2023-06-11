bec6a41b8715be64ed9dbeeee337b4096b8c9dbb
Shubham Lashkan <shubhamlashkan@gmail.com>
2023-06-11 22:20:28 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'twitter-blue': '#1DA1F2',
+        'linkedin-blue': '#0A66C2',
+        'youtube-red':'#FF0000',
+        'github-black':'#181717',
+        'instagram-purple':'#C13584',
+        'hashnode-purple':'#2962FF',
+        'gumroad-green':'#36A9AE'
+      },
+    },
+  },
+  plugins: [],
+}
